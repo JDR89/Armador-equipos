f54ee8e9fe852abffe8447e4bc814dc53827bae9
JDR89 <joaquin.romero89@gmail.com>
2023-10-24 20:54:47 -0300

    arreglo dofon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import Container from "@mui/material/Container";
 
 
 const appBarStyle = {
-  backgroundImage: 'url("../../public/futbol.jpg")',
+  backgroundImage: 'url("https://images.unsplash.com/photo-1570498839593-e565b39455fc?auto=format&fit=crop&q=80&w=2835&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
   backgroundPosition: 'center',
   backgroundSize: 'cover',
  
