262096aa806b240d3519a77f7b9fa18dcd045709
JDR89 <joaquin.romero89@gmail.com>
2023-10-31 16:15:24 -0300

    toastify

--- src/components/ClipBoard.jsx
+++ src/components/ClipBoard.jsx
@@ -2,18 +2,24 @@ import { useEffect, useState } from "react";
 import Button from '@mui/material/Button'
 // import FileCopyIcon from '@mui/icons-material/FileCopy';
 import clipboardCopy from "clipboard-copy";
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
+// minified version is also included
+// import 'react-toastify/dist/ReactToastify.min.css';
 
 
 const ClipBoard = ({team1,team2}) => {
 
    const [ListaTeamUno,setListaTeamUno] = useState(team1)
    const [ListaTeamDos,setListaTeamDos] = useState(team2)
+
    
    const arrayDeStrings = ListaTeamUno;
    const arrayDeStringsDos = ListaTeamDos
    const cadenaCopy = "--Team 1--"+"\n" + arrayDeStrings.join("\n")+"\n" +"\n" + "--Team 2--"+"\n" + arrayDeStringsDos.join("\n")
 
+   const notify = () => toast("Copiado!");
 
   const handleCopyToClipboard = async () => {
     try {
@@ -24,10 +30,13 @@ const ClipBoard = ({team1,team2}) => {
     }
   };
 
+ 
+
   useEffect(() => {
     setListaTeamUno(team1)
     setListaTeamDos(team2)
   }, [team1,team2])
+  
   
 
   return (
@@ -39,9 +48,22 @@ const ClipBoard = ({team1,team2}) => {
         color="primary"
         onClick={handleCopyToClipboard}
       >
-        <span className="text-xs">Copiar</span>
+        <span onClick={notify} className="text-xs">Copiar</span>
        {/* <FileCopyIcon /> */}
+       
       </Button>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+      />
     </div>
   );
 };
